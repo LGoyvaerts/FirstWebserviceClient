b5a7c5454357fd6d60da75333602fcee71737258
Goyvaerts Lorris <gol@TIM-M013-2.local>
2017-11-15 13:20:09 +0100

    Added cool Dieci-Pizza's including the Pictures from Dieci

--- app/src/main/java/com/apprentice/ti8m/myfirstrestclient/screens/DataAdapter.java
+++ app/src/main/java/com/apprentice/ti8m/myfirstrestclient/screens/DataAdapter.java
@@ -20,11 +20,17 @@ import java.util.List;
  * Don't copy my Stuff!
  */
 
-public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder>{
+public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
     private List<Pizza> pizzas;
 
     public DataAdapter(List<Pizza> pizzas) {
         this.pizzas = pizzas;
+    }
+
+    public static BigDecimal round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd;
     }
 
     @Override
@@ -37,11 +43,18 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder>{
     public void onBindViewHolder(DataAdapter.ViewHolder viewHolder, int i) {
 
         //viewHolder.pizza_image.setText(pizzas.get(i).getId().toString());
+        viewHolder.pizza_imagepath = pizzas.get(i).getImagepath();
         viewHolder.pizza_name.setText(pizzas.get(i).getName());
         BigDecimal result;
         result = round(pizzas.get(i).getPrice(), 2);
-        String price = "Price: "+result.toString();
+        String price = "Price: " + result.toString();
         viewHolder.pizza_price.setText(price);
+
+        GlideApp.with(viewHolder.pizza_image.getContext())
+                .load(pizzas.get(i).getImagepath())
+                .apply(RequestOptions.circleCropTransform())
+                .into(viewHolder.pizza_image);
+
     }
 
     @Override
@@ -52,24 +65,16 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder>{
     public class ViewHolder extends RecyclerView.ViewHolder {
         private ImageView pizza_image;
         private TextView pizza_name, pizza_price;
+        private String pizza_imagepath;
 
         public ViewHolder(View view) {
             super(view);
 
             pizza_image = view.findViewById(R.id.imageView);
-            GlideApp.with(view.getContext())
-                    .load(R.drawable.samplepic)
-                    .apply(RequestOptions.circleCropTransform())
-                    .into(pizza_image);
+
             pizza_name = view.findViewById(R.id.pizza_name);
             pizza_price = view.findViewById(R.id.pizza_price);
 
         }
-    }
-
-    public static BigDecimal round(float d, int decimalPlace) {
-        BigDecimal bd = new BigDecimal(Float.toString(d));
-        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
-        return bd;
     }
 }
